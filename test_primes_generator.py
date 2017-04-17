c28eb532130e3e0af3b887aedf167c1312fd5ab4
Sebuuma Daniel <sedzsoft@gmail.com>
2017-04-17 20:22:21 +0300

    Adds test to check that primes_generator returns correct list of primes.

--- test_primes_generator.py
+++ test_primes_generator.py
@@ -10,3 +10,10 @@ class TestPrimesGenerator(unittest.TestCase):
     def test_primes_generator_raises_value_error_for_non_integer_argument(self):
         self.assertRaises(ValueError, primes_generator, [1, 2])
         self.assertRaises(ValueError, primes_generator, 'string')
+
+    def test_primes_generator_raises_value_error_for_argument_less_than_2(self):
+        self.assertRaises(ValueError, primes_generator, -1)
+
+    def test_primes_generator_returns_primes_up_to_the_limit_integer_argument(self):
+        results = primes_generator(10)
+        self.assertEqual(results, [2, 3, 5, 7])
