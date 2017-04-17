1bc536b8c132e04172eb9578bb73a7fbb816322a
Sebuuma Daniel <sedzsoft@gmail.com>
2017-04-17 20:23:08 +0300

    Adds algorithm to generate a list of primes less within a given limit.

--- primes_generator.py
+++ primes_generator.py
@@ -2,5 +2,20 @@ def primes_generator(limit):
     if not isinstance(limit, int):
         raise ValueError('Limit values must be an integer')
 
-    
+    if limit < 2:
+        raise ValueError('Limit values should be greater then 1')
+
+    results = []
+    for number in range(2, limit + 1):
+        is_prime = True
+        divisor = 2
+        while divisor < number:
+            if number % divisor == 0:
+                is_prime = False
+            divisor += 1
+
+        if is_prime:
+            results.append(number)
+
+    return results
 
